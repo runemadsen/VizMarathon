8f23ad31696432cdab0cbb979438e8153cf87ee8
pholz <p.holzkorn@gmail.com>
2010-10-24 03:54:28 -0400

    consolidation

--- parse1/applet/parse1.java
+++ parse1/applet/parse1.java
@@ -1,8 +1,30 @@
-import controlP5.*;
+import processing.core.*; 
+import processing.xml.*; 
+
+import controlP5.*; 
+import toxi.geom.*; 
+import toxi.math.*; 
+
+import java.applet.*; 
+import java.awt.Dimension; 
+import java.awt.Frame; 
+import java.awt.event.MouseEvent; 
+import java.awt.event.KeyEvent; 
+import java.awt.event.FocusEvent; 
+import java.awt.Image; 
+import java.io.*; 
+import java.net.*; 
+import java.text.*; 
+import java.util.*; 
+import java.util.zip.*; 
+import java.util.regex.*; 
+
+public class parse1 extends PApplet {
+
+
 
 ControlP5 controlP5;
 Slider s;
-Habitat h;
 
 String lines[];
 int yr[];
@@ -31,7 +53,7 @@ long iterationCounter = 0;
 
 class Circle {
   public float x, y, radius;
-  public color myColor;
+  public int myColor;
   
   public Circle(float x, float y, float radius) {
     this.x = x;
@@ -80,7 +102,7 @@ class ppmModule
      
      for(int i = 0; i < lines.length; i++)
      {
-         data[i] = float(lines[i]);
+         data[i] = PApplet.parseFloat(lines[i]);
      }
   }
 }
@@ -114,7 +136,7 @@ class co2RegModule
        String spl[] = lines[i].split("\t");
        for(int j = 0; j < spl.length; j++)
        {
-          data[i][j] = float(spl[j]); 
+          data[i][j] = PApplet.parseFloat(spl[j]); 
           print(data[i][j] + " _ ");
        }
        println();
@@ -142,8 +164,8 @@ class gmslModule
     {
       String spl[] = lines[i].split("\t");
     
-      yr_raw[i] = int(spl[0].substring(4));
-      gmsl_raw[i] = float(spl[1]);
+      yr_raw[i] = PApplet.parseInt(spl[0].substring(4));
+      gmsl_raw[i] = PApplet.parseFloat(spl[1]);
     //  println(i + " " + yr_raw[i] + "/" + gmsl_raw[i]);
     }
     
@@ -163,18 +185,16 @@ class gmslModule
   } 
 }
 
-void setup()
+public void setup()
 {
   ellipseMode(CENTER);
   rectMode(CENTER);
-  size(1440, 900);
+  size(1024, 768);
   background(255);
   smooth();
   
   silk8 = loadFont("Silkscreen-8.vlw"); 
   textFont(silk8);
-  
-  h = new Habitat();
   
   gm = new gmslModule();
   co2 = new co2RegModule();
@@ -199,10 +219,10 @@ void setup()
     String spl[] = lines[i].split(";");
 
 
-    yr[i] = int(spl[0]);
-    total[i] = float(spl[1]);
-    plus[i] = float(spl[3]);
-    rate[i] = float(spl[2]);
+    yr[i] = PApplet.parseInt(spl[0]);
+    total[i] = PApplet.parseFloat(spl[1]);
+    plus[i] = PApplet.parseFloat(spl[3]);
+    rate[i] = PApplet.parseFloat(spl[2]);
 
   //  println(plus[i]);
 
@@ -222,7 +242,7 @@ void setup()
  circles = createPackC(); 
 }
 
-void update()
+public void update()
 {
  // println(sliderVal);
   
@@ -231,7 +251,7 @@ void update()
   
 }
 
-ArrayList createPackC()
+public ArrayList createPackC()
 {
   ArrayList circles = new ArrayList();
   //colorMode(HSB, 255);
@@ -264,16 +284,11 @@ ArrayList createPackC()
  
 }
 
-void draw()
+public void draw()
 {
   update();
   
-  
-  
   background(255);
-  
-  h.setYear(sliderVal);
-  h.display();
   
  // noStroke();
   
@@ -296,26 +311,12 @@ void draw()
      arc(width/2, height/2, val, val, i * 2*PI/12 - PI/2, (i+1) * 2*PI/12 - PI/2);
   }
   
-  
-  
 
   // PLANET
   strokeWeight(2);
   stroke(color(40));
   fill(color(240));
   ellipse(width/2, height/2, 2*r, 2*r);
-  
-  // TEMP
-  tempViz.setYear(min(sliderVal, 2010));
-  pushMatrix();
-  translate(width/2, height/2);
-  scale(2.0f);
-  tempViz.display();
-  popMatrix();
-  
-  noStroke();
-  noFill();
-  colorMode(RGB, 255);
   
   // ATMOSPHERE PPM
   
@@ -328,20 +329,20 @@ void draw()
       ppmind = sliderVal - 1980;
       
   ellipse(width/2, height/2, 2*r + ppm.data[min(ppmind, 29)]/2.0f, 2*r + ppm.data[min(ppmind, 29)]/2.0f);
-  line(width/2 + cos(PI/3) * (r + ppm.data[min(ppmind, 29)]/4.0f), height/2 - sin(PI/3) * (r + ppm.data[min(ppmind, 29)]/4.0f), width/2 + cos(PI/3) * 1.3 *(r + ppm.data[min(ppmind, 29)]/4.0f), height/2 - sin(PI/3) * 1.3 * (r + ppm.data[min(ppmind, 29)]/4.0f));
+  line(width/2 + cos(PI/3) * (r + ppm.data[min(ppmind, 29)]/4.0f), height/2 - sin(PI/3) * (r + ppm.data[min(ppmind, 29)]/4.0f), width/2 + cos(PI/3) * 1.3f *(r + ppm.data[min(ppmind, 29)]/4.0f), height/2 - sin(PI/3) * 1.3f * (r + ppm.data[min(ppmind, 29)]/4.0f));
   fill(color(40));
   noStroke();
   
-  text(ppm.data[min(ppmind, 29)] + " parts per million CO2 concentration", width/2 + cos(PI/3) * 1.3 *(r + ppm.data[min(ppmind, 29)]/4.0f), height/2 - sin(PI/3) * 1.3 * (r + ppm.data[min(ppmind, 29)]/4.0f));
+  text(ppm.data[min(ppmind, 29)] + " parts per million CO2 concentration", width/2 + cos(PI/3) * 1.3f *(r + ppm.data[min(ppmind, 29)]/4.0f), height/2 - sin(PI/3) * 1.3f * (r + ppm.data[min(ppmind, 29)]/4.0f));
   
   noFill();
   stroke(color(255, 20, 20));
   ellipse(width/2, height/2, 2*r + 350.0f/2.0f, 2*r + 350.0f/2.0f);
-  line(width/2 + cos(PI/3.5f) * (r + 350.0f/4.0f), height/2 - sin(PI/3.5f) * (r + 350.0f/4.0f), width/2 + cos(PI/3.5f) * 1.3 *(r + 350.0f/4.0f), height/2 - sin(PI/3.5f) * 1.3 * (r + 350.0f/4.0f));
+  line(width/2 + cos(PI/3.5f) * (r + 350.0f/4.0f), height/2 - sin(PI/3.5f) * (r + 350.0f/4.0f), width/2 + cos(PI/3.5f) * 1.3f *(r + 350.0f/4.0f), height/2 - sin(PI/3.5f) * 1.3f * (r + 350.0f/4.0f));
   fill(color(40));
   noStroke();
   
-  text(350.0f + " boundary CO2 parts per million.", width/2 + cos(PI/3.5f) * 1.3 *(r + 350.0f/4.0f), height/2 - sin(PI/3.5f) * 1.3 * (r + 350.0f/4.0f));
+  text(350.0f + " boundary CO2 parts per million.", width/2 + cos(PI/3.5f) * 1.3f *(r + 350.0f/4.0f), height/2 - sin(PI/3.5f) * 1.3f * (r + 350.0f/4.0f));
   
   
 
@@ -397,13 +398,16 @@ void draw()
   text(("" + co2sum/10.0f + " tons personal co2 emission "), 30, -co2sum);
   popMatrix();
   
-  
-  
+  tempViz.setYear(sliderVal);
+  pushMatrix();
+  translate(width/2, height/2);
+  popMatrix();
+  tempViz.display();
  
   
 }
 
-void mouseReleased()
+public void mouseReleased()
 {
    // circles = createPackC(); 
 }
@@ -421,7 +425,7 @@ Comparator comp = new Comparator() {
     }
 };
 
-void iterateLayout(int iterationCounter) {
+public void iterateLayout(int iterationCounter) {
 
   Object circs[] = circles.toArray();
   Arrays.sort(circs, comp);
@@ -439,13 +443,13 @@ void iterateLayout(int iterationCounter) {
         float dy = cj.y - ci.y;
         float r = ci.radius + cj.radius;
         float d = (dx*dx) + (dy*dy);
-        if (d < (r * r) - 0.01 ) {
+        if (d < (r * r) - 0.01f ) {
 
           v.x = dx;
           v.y = dy;
 
           v.normalize();
-          v.mult((r-sqrt(d))*0.5);
+          v.mult((r-sqrt(d))*0.5f);
 
             cj.x += v.x;
             cj.y += v.y;
@@ -458,7 +462,7 @@ void iterateLayout(int iterationCounter) {
   }
 
   //Contract
-  float damping = 0.1/(float)(iterationCounter);
+  float damping = 0.1f/(float)(iterationCounter);
   for (int i=0; i<circs.length; i++) {
     Circle c = (Circle)circs[i];
       v.x = c.x-width/2;
@@ -469,7 +473,7 @@ void iterateLayout(int iterationCounter) {
   }
 }
 
-ArrayList createRandomCircles(int n) {
+public ArrayList createRandomCircles(int n) {
   ArrayList circles = new ArrayList();
   colorMode(HSB, 255);
   while (n-- > 0) {
@@ -481,7 +485,7 @@ ArrayList createRandomCircles(int n) {
   return circles;
 }
 
-void controlEvent(ControlEvent theEvent) {
+public void controlEvent(ControlEvent theEvent) {
   switch(theEvent.controller().id()) {
     case(1):
      circles = createPackC(); 
@@ -489,3 +493,100 @@ void controlEvent(ControlEvent theEvent) {
   }
 }
 
+public abstract class AbstractVisualizer
+{
+   public abstract void setYear(int year);
+   public abstract void display();
+   public abstract String getTitle();
+   public abstract boolean setCountry(String countryCode);
+}
+
+
+
+public class TemperatureVisualizer extends AbstractVisualizer
+{
+  private HashMap data = new HashMap();
+  private int curYear = 2009;
+//  private InterpolateStrategy is=new CircularInterpolation(true);
+
+  public TemperatureVisualizer() {
+    parseCsv();
+  }
+  
+  float ang = 0, target = PI;
+
+  private void parseCsv() {
+    String[] lines = loadStrings("temperature.txt");
+
+    for(int i = 1; i < lines.length; i++) {
+      String chunks[] = lines[i].split(",");
+      int year = Integer.parseInt(chunks[0].trim());
+      float[] monthtemps = new float[12];
+      for (int m=1; m < monthtemps.length; m++) {
+        if (m >= chunks.length)
+          monthtemps[m-1] = monthtemps[m-2];
+        else
+          monthtemps[m-1] = /*57.2+1.8**//* 14.0 +*/30 + Float.parseFloat(chunks[m]) /*/100*/;
+      }
+
+      data.put(year, monthtemps);
+    }
+  }
+
+  public void setYear(int year) {
+    curYear = year;
+  }
+
+  public void display() {
+    smooth();
+    noStroke();
+    colorMode(HSB);
+  
+    float[] thisYear = (float[])data.get(curYear);
+    
+    float avg = 0;
+    int n = 0;
+    for (float f : thisYear) {
+      avg += f;
+      if (f != 0) n++;
+    }
+    avg /= n;
+    
+//    ang = is.interpolate(ang, target, 0.4);
+//    rotateY(ang);
+//    if (abs(degrees(ang)%180-180) < 10) target += PI;
+ 
+    Spline2D s = new Spline2D();
+    s.add(new Vec2D(avg/2, radians(-26)).toCartesian());
+    for (int i = 0; i < n; i++) {
+      Vec2D a = new Vec2D(avg/2, i*TWO_PI/n + radians(-5)).toCartesian();
+      Vec2D b = new Vec2D(avg/2 + thisYear[i]/2, i*TWO_PI/n).toCartesian();
+      Vec2D c = new Vec2D(avg/2, i*TWO_PI/n + radians(5)).toCartesian();
+      s.add(a).add(b).add(c);
+    }
+    
+    stroke(30, 190, 250);
+    fill(30, 250, 250);
+    strokeWeight(2);
+    beginShape();
+    for (Vec2D v : s.computeVertices(20)) {
+      vertex(v.x, v.y);
+    //ellipse(v.x, v.y,1,1);
+    }
+    
+    endShape();
+  }
+
+  public String getTitle() {
+    return "Global Temperature";
+  }
+
+  public boolean setCountry(String countryCode) {
+    return false;
+  }
+}
+
+  static public void main(String args[]) {
+    PApplet.main(new String[] { "--bgcolor=#FFFFFF", "parse1" });
+  }
+}
