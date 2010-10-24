644cba6f20165889192afcf0d200ccc3dc54f02a
malachi <malachi@malachis-macbook-2.local>
2010-10-24 06:55:36 -0400

    tims commit for some stupid reason

--- timsVisualization/src/MyClass.java
+++ timsVisualization/src/MyClass.java
@@ -1,30 +1,30 @@
 
 
-	class MyClass extends AbstractVisualizer
-	{ 
-	  void MyClass()
-	  {
-	     
-	  } 
-	  
-	  void msetYear(int year) 
-	  {
-	  }
-	  
-	  void display()
-	  {
-	  }
-	  
-	  String getTitle()
-	  {
-	    return "My Name";
-	  }
-	  
-	  boolean setCountry(String countryCode)
-	  {
-	    return false;
-	  }
-	  
-	  
+class MyClass extends AbstractVisualizer
+{ 
+	void MyClass()
+	{
+	} 
+
+	void msetYear(int year) 
+	{
 	}
+
+	void display()
+	{
+		
+	}
+
+	String getTitle()
+	{
+		return "My Name";
+	}
+
+	boolean setCountry(String countryCode)
+	{
+		return false;
+	}
+
+
+}
 
